1111beaaefa82bbbf1cec81b6239ea4bb99103bc
jeenie <jeenie828@gmail.com>
2018-07-11 23:56:45 +0900

    DI_examples

---
+++ spring_5_2_ex1_springex/src/main/java/com/javalec/ex/Pencil6BWithEraser.java
@@ -0,0 +1,9 @@
+package com.javalec.ex;
+
+public class Pencil6BWithEraser extends Pencil6B implements Pencil{
+	@Override
+	public void use() {
+		//super.user()
+		System.out.println("6B굵기로 쓰이고, 지우개가 있습니다.");
+	}
+}
